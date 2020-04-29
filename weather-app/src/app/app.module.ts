8fc09e65a4b28b3a2eccb32261c7135d1f1dabe0
Toby Drane <tobydrane@gmail.com>
2020-04-29 19:12:39 +0100

    added graph

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -2,16 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ChartsModule } from 'ng2-charts';
+
 import { AppComponent } from './components/app/app.component';
 import { TableComponent } from './components/table/table.component';
 import { PaginatePipe } from './pipes/paginate/paginate.pipe';
 import { PaginateComponent } from './components/paginate/paginate.component';
 import { FilterPipe } from './pipes/filter/filter.pipe';
 import { FilterComponent } from './components/filter/filter.component';
+import { GraphComponent } from './components/graph/graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GraphComponent,
     TableComponent,
     PaginatePipe,
     PaginateComponent,
@@ -21,6 +25,7 @@ import { FilterComponent } from './components/filter/filter.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ weather-app/src/app/components/graph/graph.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SingleWeatherData } from 'src/app/types';
+import { ChartType, ChartDataSets, ChartOptions, ChartData } from 'chart.js';
+
+@Component({
+  selector: 'app-graph',
+  template: `
+    <div class="graph">
+      <h1>Weather Graph</h1>
+      <canvas baseChart
+        [datasets]="chartData"
+        [chartType]="barChartType"
+        [options]="barChartOptions"
+        [labels]="barChartLabels"
+      ></canvas>
+    </div>
+  `,
+  styleUrls: ['./graph.component.scss'],
+})
+export class GraphComponent implements OnInit {
+  @Input()
+  public data: Observable<SingleWeatherData[]>;
+
+  // The chart stuff
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartLabels = [];
+  public chartData = [
+    { data: [], label: 'Temperature' },
+    { data: [], label: 'Mean Temperature' },
+  ];
+
+  public ngOnInit(): void {
+    this.data.subscribe(d => {
+      let totalTemp = 0;
+      d.forEach((city) => {
+        this.chartData[0].data.push(city.main.temp);
+        this.barChartLabels.push(city.name);
+        totalTemp += city.main.temp;
+      });
+      // Now add mean
+      const mean = totalTemp / d.length;
+      this.chartData[1].data.push(mean);
+    });
+  }
+}
